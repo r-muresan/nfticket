e19272d6e3305db23f26a7d704e0c6f1f881c93b
Anna Mouland <anna.electros@gmail.com>
2022-06-25 16:39:20 -0400

    fix contract bug

--- hardhat/contracts/Governance.sol
+++ hardhat/contracts/Governance.sol
@@ -17,9 +17,8 @@ contract Governance is Ownable{
     mapping(uint256 => mapping(address => bool)) hasVoted;
     //proposal id -> user addr -> has voted?
     mapping(uint256 => uint256) proposalToEvent;
-    //proposal id -> event id for checking voter eligibility
-    //mapping(uint256 => Proposal) proposalById;
-    //proposal id -> Proposal
+    mapping(uint256 => uint256) numProposals;
+    //eventId --> num proposals for that event
     mapping(uint256 => Proposal) proposalInfo;
     uint256 proposalID;
 
@@ -29,23 +28,23 @@ contract Governance is Ownable{
         string[] options;
         uint256 voteDelay;
         uint256 creationTime;
-        mapping(string => uint256) votes;
+        uint256[] votes;
     }
 
     function submitProposal(uint256 _eventId, string memory _proposing, uint256 _voteDelay, string[] memory _options) public {
 
         require(nfticket.getEventOwner(_eventId) == msg.sender, "caller is not the event owner");
 
+        uint256[] memory _votes;
         proposalInfo[proposalID].id = _eventId;
         proposalInfo[proposalID].proposing = _proposing;
         proposalInfo[proposalID].options= _options;
         proposalInfo[proposalID].voteDelay = _voteDelay;
         proposalInfo[proposalID].creationTime = block.timestamp;
-        for(uint256 i; i < _options.length; i++){
-            proposalInfo[proposalID].votes[_options[i]] = 0;
-        }
+        proposalInfo[proposalID].votes = _votes;
         
         proposalToEvent[proposalID] = _eventId;
+        numProposals[_eventId]++;
     }
 
     function vote(uint256 _proposalId, string memory _option) public {
@@ -56,9 +55,16 @@ contract Governance is Ownable{
         require(hasVoted[_proposalId][msg.sender] = false, "you have already voted");
         require(nfticket.didUserBuy(_event, msg.sender) == true, "you don't have a ticket for this event");
 
-        Proposal storage _proposal = proposalInfo[_proposalId];
+        Proposal memory _proposal = proposalInfo[_proposalId];
 
-        _proposal.votes[_option] += 1;
+        uint256 index; 
+        for(uint256 i ; i < _proposal.options.length; i++){
+            if(keccak256(abi.encodePacked(_proposal.options[i])) == keccak256(abi.encodePacked(_option))){
+                index = i;
+            }
+        }
+
+        _proposal.votes[index] += 1;
 
         hasVoted[_proposalId][msg.sender] = true;
     }
@@ -66,15 +72,17 @@ contract Governance is Ownable{
     function getVoteWinner(uint256 _proposalId) public returns(string memory){
         uint256 votes;
         string memory chosenOption;
+
+        Proposal storage _proposal = proposalInfo[_proposalId];
         
         uint256 _creationTime = proposalInfo[_proposalId].creationTime;
         uint256 _voteDelay = proposalInfo[_proposalId].voteDelay;
         require(_voteDelay + _creationTime < block.timestamp, "voting period still open");
 
         string[] memory props = _proposal.options;
-        for(uint256 i; i < _proposal.options.length; i ++){
-            if(_proposal.votes[props[i]] > votes){
-                votes = _proposal.votes[props[i]];
+        for(uint256 i; i < _proposal.votes.length; i ++){
+            if(_proposal.votes[i] > votes){
+                votes = _proposal.votes[i];
                 chosenOption = _proposal.options[i];
             }
         }
@@ -82,10 +90,14 @@ contract Governance is Ownable{
     }
 
     function getProposals(uint256 _eventId) public view returns(Proposal[] memory){
-        uint256[] memory proposalIds = new uint256[](0);
-        for(uint256 i; i < proposalToEvent.length; i++){
+        uint256[] memory proposalIds = new uint256[](numProposals[_eventId]);
+        uint256 index;
+
+        
+        for(uint256 i; i < numProposals[_eventId]; i++){   
             if(proposalToEvent[i] == _eventId){
-                proposalIds.push(i);
+                proposalIds[index] = i;
+                index++;
             }
         }
         Proposal[] memory proposals = new Proposal[](proposalIds.length);
